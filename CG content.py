c2b71cf950d4e174aac250af1fd07bb0ac672d26
AmirMahfouz <101039892+AmirMahfouz@users.noreply.github.com>
2023-03-28 20:23:58 +0200

    Add files via upload

---
+++ CG content.py
@@ -0,0 +1,11 @@
+DNA_Seq = input("Enter Your DNA Sequence :")
+C = DNA_Seq.count('C')
+G = DNA_Seq.count('G')
+CG_Content = (C+G)/len(DNA_Seq)*100
+print(CG_Content)
+
+
+
+
+
+input("Press ENTEr to EXIT ..!")
